53df1bf90954a824456e2667954985d765a64d64
mr-mukhtar <mukhtarmohd517@gmail.com>
2023-08-14 16:11:27 +0530

    updated

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
+"use strict";
 var form = document.getElementById('my-form');
 var itemList = document.getElementById('items');
 var userDetailsList = [];
-var totalValue = 0; // Variable to store the total value
+var totalValue = 0.00; // Variable to store the total value
 
 form.addEventListener('submit', onSubmit);
 itemList.addEventListener('click', deleteItem); // Add event listener to the item list
-
-function onSubmit(e) {
+async function onSubmit(e) {
     e.preventDefault();
-    var price = parseFloat(document.getElementById('selling').value); // Parse float to handle decimal values
+    var price = parseFloat(document.getElementById('selling').value);
     var name = document.getElementById('product').value;
     var li = createListItem(price, name);
     itemList.appendChild(li);
@@ -18,26 +18,21 @@ function onSubmit(e) {
         name: name
     }
 
-    // Save the new item to the API
-    axios.post("https://crudcrud.com/api/10e142c7a027485e92625067a8c1fe4e/sellingprice", userDetails)
-        .then((res) => {
-            console.log(res);
-            // Update the JavaScript variable with the new data
-            userDetails._id = res.data._id;
-            userDetailsList.push(userDetails);
-            // Update totalValue by adding the new price
-            totalValue += price;
-            // Update the total value in the API
-            updateTotalValueInAPI(totalValue);
-            // Update the total value in the UI
-            updateTotalValueUI(totalValue);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    try {
+        const response = await axios.post("https://crudcrud.com/api/2c97b74e219f462b83d1a8fcd8234f3d/sellingprice", userDetails);
+        console.log(response);
+        userDetails._id = response.data._id;
+
+        totalValue += price;
+        await updateTotalValueInAPI(totalValue);
+        updateTotalValueUI(totalValue);
+    } catch (error) {
+        console.log(error);
+    }
 
     form.reset();
 }
+ 
 
 function createListItem(price, name, itemId) {
     var li = document.createElement('li');
@@ -54,44 +49,36 @@ function createListItem(price, name, itemId) {
     return li;
 }
 
-function deleteItem(e) {
+async function deleteItem(e) {
     if (e.target.classList.contains('delete')) {
         if (confirm('Are you sure you want to delete this product?')) {
             var li = e.target.parentElement;
             var listItemPrice = parseFloat(li.childNodes[0].textContent);
             itemList.removeChild(li);
 
-            // Delete the item from the API
             var itemId = li.getAttribute('data-item-id');
-            axios.delete(`https://crudcrud.com/api/10e142c7a027485e92625067a8c1fe4e/sellingprice/${itemId}`)
-                .then((res) => {
-                    console.log(res);
-                    // Update the JavaScript variable after successful deletion
-                    userDetailsList = userDetailsList.filter(user => user._id !== itemId);
-                    // Update totalValue by subtracting the deleted price
-                    totalValue -= listItemPrice;
-                    // Update the total value in the API
-                    updateTotalValueInAPI(totalValue);
-                    // Update the total value in the UI
-                    updateTotalValueUI(totalValue);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+
+            try {
+                const response = await axios.delete(`https://crudcrud.com/api/2c97b74e219f462b83d1a8fcd8234f3d/sellingprice/${itemId}`);
+                console.log(response);
+                totalValue -= listItemPrice;
+                await updateTotalValueInAPI(totalValue);
+                updateTotalValueUI(totalValue);
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 }
 
-function updateTotalValueInAPI(value) {
-    // Update the total value in the API
-    
-    axios.put(`https://crudcrud.com/api/10e142c7a027485e92625067a8c1fe4e/totalvalue`, { totalValue: value })
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+
+async function updateTotalValueInAPI(value) {
+    try {
+        const response = await axios.put(`https://crudcrud.com/api/2c97b74e219f462b83d1a8fcd8234f3d/totalvalue`, { totalValue: value });
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 function updateTotalValueUI(value) {
@@ -101,30 +88,27 @@ function updateTotalValueUI(value) {
     totalValueElement.textContent = 'Total Value Worth of Products: Rs ' + value.toFixed(2);
 }
 
-function loadItems() {
-    // Fetch data from the API and display the list items
-    axios.get("https://crudcrud.com/api/10e142c7a027485e92625067a8c1fe4e/sellingprice")
-        .then((response) => {
-            var data = response.data;
-            userDetailsList = data; // Update the JavaScript variable with the fetched data
-            // Create and append list items using a for loop
-            for (var i = 0; i < data.length; i++) {
-                var user = data[i];
-                var li = createListItem(user.price, user.name, user._id);
-                li.setAttribute('data-item-id', user._id);
-                itemList.appendChild(li);
-                // Calculate totalValue when loading items
-                totalValue += user.price;
-            }
-            // Update the total value in the UI
-            updateTotalValueUI(totalValue);
-            // Update the total value in the API
-            updateTotalValueInAPI(totalValue);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+async function loadItems() {
+    try {
+        const response = await axios.get("https://crudcrud.com/api/2c97b74e219f462b83d1a8fcd8234f3d/sellingprice");
+        var data = response.data;
+        userDetailsList = data;
+
+        for (var i = 0; i < data.length; i++) {
+            var user = data[i];
+            var li = createListItem(user.price, user.name, user._id);
+            li.setAttribute('data-item-id', user._id);
+            itemList.appendChild(li);
+            totalValue += user.price;
+        }
+        updateTotalValueUI(totalValue);
+        await updateTotalValueInAPI(totalValue);
+    } catch (error) {
+        console.log(error);
+    }
 }
+
 
 // Load items from the API when the page loads
 loadItems();
+//updated  code
